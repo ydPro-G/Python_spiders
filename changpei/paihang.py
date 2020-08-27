ddcbca5e262e30597f4634d6a183a48cd7271833
ydPro-G <46178109+ydPro-G@users.noreply.github.com>
2020-08-27 13:47:25 +0800

    下一页操作

--- changpei/paihang.py
+++ changpei/paihang.py
@@ -6,13 +6,15 @@ from selenium.common.exceptions import StaleElementReferenceException
 wb = webdriver.Chrome()
 
 wb.get('https://www.gongzicp.com/home/ranking?tid=75&id=1&rankList=2&date=3')
-tag = len(wb.find_elements_by_xpath(
-    '//div[@class="cp-novel-list"]//a[@class="cp-novel-name"]'))
-filename = 'cp.txt'
+all = wb.find_elements_by_xpath('//a[@class="cp-novel-name"]')
+s = len(all)
+filename = 'changpei.txt'
+for i in range(s):
+    all[i].click() # 获取列表元素的索引，元素索引从0开始依次点击
+    wb.back()
+    sleep(1)
+    all = wb.find_elements_by_xpath('//a[@class="cp-novel-name"]')
 
-for i in range(tag):
-    wb.find_element_by_xpath('//div[@class="cp-novel-list"]//div[@class="right"]//div[@class="cp-over-hidden"]//a[i+1]').click()
-    sleep(0.1)
     for handle in wb.window_handles:
         wb.switch_to_window(handle)
         if '长佩文学网' in handle:
@@ -25,5 +27,3 @@ for i in range(tag):
             file_t = t.text
             f_n.write(file_t)
         f_n.write('\n')
-    wb.back()
-    sleep(5)
